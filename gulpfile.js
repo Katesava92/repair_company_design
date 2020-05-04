25698cf2bc2e6540cfd4c049f4fc75644959a145
Kate <katesava92@gmail.com>
2020-05-04 13:40:24 +0300

    install cleanCss & rename plugins & create minify task

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,29 @@ function defaultTask(cb) {
 exports.default = defaultTask
 
 const gulp = require("gulp");
-const browserSync = require('browser-sync').create();
+const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const renameCss = require("gulp-rename");
+
+
 
 gulp.task("hello", function (done) {
   console.log("Привет, мир!");
   done();
 });
 
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function() {
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task("minify", function() {
+  return gulp.src("./css/*.css")
+    .pipe(cleanCSS())
+    .pipe(renameCss({suffix:".min"}))
+    .pipe(gulp.dest("./css"));
 });
