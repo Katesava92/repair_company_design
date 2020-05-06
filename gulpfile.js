b6cafb738e4d76819d511e470db16c602f6a1981
Kate <katesava92@gmail.com>
2020-05-06 22:21:04 +0300

    feat: install gulp-autoprefixer & rewrite gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const browserSync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const renameCss = require("gulp-rename");
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 
 
@@ -31,15 +32,13 @@ function bs() {
 function serveSass() {
   return src("./sass/*.sass")
       .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+    }))
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
 };
 exports.serve = bs;
-
-
-
-
-
 
 
 function min() {
