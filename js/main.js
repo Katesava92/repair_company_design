016b8776b3beade7d0e0133758ec2da5bfb0013d
Kate <katesava92@gmail.com>
2020-05-15 22:32:44 +0300

    refact: adaptive for design and types sections & feat: add js code for modal dialog

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(event) {  //проверка прогрузилась ли страница
+  const modal = document.querySelector('.modal'); // переменная кот находит элемент с классом modal
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]'); // переменная которая находит все элементы с атрибутом
+  const toggleModal = () => {
+    modal.classList.toggle('modal--visible');
+  } //функция которая переключает класс, включает если его нет, и выкл если он есть
+  const closeBtn = document.querySelector('.modal__close');
+
+
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
+  closeBtn.addEventListener('click',toggleModal);
+
+
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 27) {
+      toggleModal;
+    }
+    
+  });
+
+
+
+});
+
+
