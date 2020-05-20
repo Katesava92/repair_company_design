ad077916916f10348a118d1298dcd7be81b4670c
Kate <katesava92@gmail.com>
2020-05-20 17:43:03 +0300

    refact: rewrite control section and adaptive to it & feat: add animation

--- js/main.js
+++ js/main.js
@@ -50,12 +50,12 @@ $(document).ready(function () {
     }
   });
 
-  /*(document).on('click', function(window) { //при нажатии вне окна закрывается оно
-      if (window.target == modal) {
+ $(document).on('click', function(e) { //при нажатии вне окна закрывается оно
+      if ( modal.is(e.target) ) {
         modal.toggleClass('modal--visible');
-    }
+    };
       
-  });*/
+  });
 
   //кнопка наверх
   var up = $('.button-up');
@@ -91,20 +91,22 @@ $(document).ready(function () {
   next.css('left', prev.width() + 30 + bullets.width() + 40)
   bullets.css('left', prev.width() + 30)
 
-  /*var mySwiper2 = new Swiper ('.swiper-container-s2', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      el: '.swiper-pagination-fraction',
-      type: 'fraction',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });*/
-
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animate__animated', // animation css class (default is animated)
+      offset:       30,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null,    // optional scroll container selector, otherwise use window,
+      resetAnimation: true,     // reset animation on end (default is true)
+    }
+  );
+  wow.init();
 
 });
 
